ccadfe720176877f82562ecc71458bea5def310b
ottolitjens <litjens.otto@gmail.com>
2015-11-22 18:41:24 -0200

    added scss support and base mainMenu and subMenu

--- client/app.ts
+++ client/app.ts
@@ -1,1 +1,14 @@
-document.writeln('This is working...')
+/// <reference path="../typings/angular2-meteor.d.ts" />
+
+import {Component, View} from 'angular2/angular2';
+import {bootstrap} from 'angular2-meteor';
+
+@Component({
+	selector: 'app'
+})
+@View({
+	templateUrl:'client/mainMenu/mainMenu.html'
+})
+class app {};
+//ss
+bootstrap(app)
